c6dc92331d6f497a5098c3a23bbd37f76b01082d
DEADB17 <DEADB17@gmail.com>
2018-02-24 09:12:59 -0500

    use union type for Db

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -6,3 +6,25 @@ type Lt = 3;
 declare type DirElm = Up | Rt | Dn | Lt;
 
 declare type OtherDirVec = [DirElm, DirElm, DirElm];
+
+
+
+type DbStage1 = 1;
+type DbStage2 = 2;
+
+interface Db {
+    mainCanvas: HTMLCanvasElement;
+    stage: createjs.StageGL;
+    npc: createjs.Bitmap[];
+    maze: createjs.Bitmap;
+}
+
+declare interface Db1 extends Db {
+    kind: DbStage1;
+}
+
+declare interface Db2 extends Db {
+    kind: DbStage2;
+    ctx: CanvasRenderingContext2D;
+    tickerHandle: Function;
+}
